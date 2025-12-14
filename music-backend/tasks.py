136857a174d3230d5910c8dcb84791706117f51d
Anik Sutradhar <aniksutradhar1official@gmail.com>
2025-12-14 13:28:38 +0530

    Update tasks.py

--- music-backend/tasks.py
+++ music-backend/tasks.py
@@ -7,8 +7,8 @@ import analyzer
 load_dotenv()
 
 # Config
-CELERY_BROKER_URL = os.getenv('CELERY_BROKER_URL', 'redis://localhost:6379/0')
-CELERY_RESULT_BACKEND = os.getenv('CELERY_RESULT_BACKEND', 'redis://localhost:6379/0')
+CELERY_BROKER_URL = os.getenv('CELERY_BROKER_URL', 'redis://redis:6379/0')
+CELERY_RESULT_BACKEND = os.getenv('CELERY_RESULT_BACKEND', 'redis://redis:6379/0')
 
 celery = Celery(__name__, broker=CELERY_BROKER_URL, backend=CELERY_RESULT_BACKEND)
 
